bc826a7e575893402545d03f347bd76bbb7d2219
Bhaskara Sai Vamsi Krishna Padala <152455231+pbsvk@users.noreply.github.com>
2025-02-19 17:19:53 -0500

    Crucial edits

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const Search = ({searchTerm, setSearchTerm}) => {
-    return (
-        <div className="search">
-            <div>
-            <img src = "../public/search.svg" alt = "Search for Movies" />
-                <input
-                    type="search"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    placeholder="Search Movies here"
-                    />
-            </div>
-        </div>
-    );
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="/search.svg" alt="Search for Movies" />
+        <input
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search Movies here"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
